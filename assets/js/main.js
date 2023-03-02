95b0dd49690c09e946ba7db41e6e3df12f2b7231
BenjaminHoefer <benjamin.hoefer@gmx.de>
2023-03-02 14:33:24 +0100

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function pictureRotation() {
+
+    /* Get the element by class name and and add another class to classlist for adding animation */
+    const picture = document.getElementsByClassName("intro__container-img")[0];
+    picture.classList.toggle("animation");
+}
